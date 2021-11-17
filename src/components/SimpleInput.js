3a3fb78c116d8f5e06e3a12b0ea0648b9052b470
Henry Salliss <henrysalliss@gmail.com>
2021-11-17 19:07:44 +0000

    tracking user input with both state and refs in SimpleInput.js

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,34 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState("");
+
+  const nameRef = useRef();
+
+  const nameChangeHandler = (e) => {
+    setName(e.target.value);
+  };
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    const enteredValue = nameRef.current.value;
+    console.log(enteredValue);
+
+    console.log(name);
+    setName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameRef}
+          type="text"
+          id="name"
+          onChange={nameChangeHandler}
+          value={name}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
